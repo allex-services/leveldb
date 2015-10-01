14ceacf2c3a1a8441754864351d44786ecf0a138
Andrija Petrovic <andrija.hers@gmail.com>
2015-10-01 18:37:28 +0200

    Support for initially empty dbs. In other words, rm -rf the db before creating it again

--- servicecreator.js
+++ servicecreator.js
@@ -1,4 +1,5 @@
-var levelup = require('level');
+var levelup = require('level'),
+  child_process = require('child_process');
 
 function createLevelDBService(execlib, ParentServicePack) {
   'use strict';
@@ -15,9 +16,14 @@ function createLevelDBService(execlib, ParentServicePack) {
 
   function LevelDBService(prophash) {
     ParentService.call(this, prophash);
-    this.db = levelup(prophash.dbname, lib.extend({}, prophash.dbcreationoptions));
-    this.dbput = q.nbind(this.db.put, this.db);
-    this.dbget = q.nbind(this.db.get, this.db);
+    this.db = null;
+    this.dbput = null;
+    this.dbget = null;
+    if (prophash.initiallyemptydb) {
+      child_process.exec('rm -rf '+prophash.dbname, this.createDB.bind(this, prophash));
+    } else {
+      this.createDB(prophash);
+    }
   }
   
   ParentService.inherit(LevelDBService, factoryCreator);
@@ -27,6 +33,19 @@ function createLevelDBService(execlib, ParentServicePack) {
     this.dbput = null;
     this.db = null;
     ParentService.prototype.__cleanUp.call(this);
+  };
+
+  LevelDBService.prototype.isInitiallyReady = function (propertyhash) {
+    return !propertyhash.initiallyemptydb;
+  };
+
+  LevelDBService.prototype.createDB = function (prophash) {
+    this.db = levelup(prophash.dbname, lib.extend({}, prophash.dbcreationoptions));
+    this.dbput = q.nbind(this.db.put, this.db);
+    this.dbget = q.nbind(this.db.get, this.db);
+    if (this.readyToAcceptUsersDefer) {
+      this.readyToAcceptUsersDefer.resolve(true);
+    }
   };
 
   LevelDBService.prototype.propertyHashDescriptor = {
