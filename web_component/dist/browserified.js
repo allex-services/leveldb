ad277c09af05fe7963e661fb1264c5df071f39c0
Andrija Petrovic <andrija.hers@gmail.com>
2016-03-01 11:53:51 +0100

    updating web_component

---
+++ web_component/dist/browserified.js
@@ -0,0 +1,95 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+ALLEX.execSuite.registry.add('allex_leveldbservice',require('./clientside')(ALLEX, ALLEX.execSuite.registry.get('.')));
+
+},{"./clientside":2}],2:[function(require,module,exports){
+function createClientSide(execlib) {
+  'use strict';
+  var execSuite = execlib.execSuite,
+  ParentServicePack = execSuite.registry.get('.');
+
+  return {
+    SinkMap: require('./sinkmapcreator')(execlib, ParentServicePack)
+  };
+}
+
+module.exports = createClientSide;
+
+},{"./sinkmapcreator":5}],3:[function(require,module,exports){
+module.exports = {
+};
+
+},{}],4:[function(require,module,exports){
+module.exports = {
+  read: [{
+    title: 'Read stream Options',
+    type: 'object'
+  }],
+  getControllableReadStream: [{
+    title: 'Read stream Options',
+    type: 'object'
+  }],
+  readStreamControl: [{
+    title: 'Read stream id',
+    type: 'string'
+  },{
+    title: 'Method name',
+    type: 'string'
+  },{
+    title: 'Array of parameters',
+    type: 'array'
+  }]
+};
+
+},{}],5:[function(require,module,exports){
+function sinkMapCreator(execlib, ParentServicePack) {
+  'use strict';
+  var sinkmap = new (execlib.lib.Map), ParentSinkMap = ParentServicePack.SinkMap;
+  sinkmap.add('service', require('./sinks/servicesinkcreator')(execlib, ParentSinkMap.get('service')));
+  sinkmap.add('user', require('./sinks/usersinkcreator')(execlib, ParentSinkMap.get('user')));
+  
+  return sinkmap;
+}
+
+module.exports = sinkMapCreator;
+
+},{"./sinks/servicesinkcreator":6,"./sinks/usersinkcreator":7}],6:[function(require,module,exports){
+function createServiceSink(execlib, ParentSink) {
+  'use strict';
+  if (!ParentSink) {
+    ParentSink = execlib.execSuite.registry.get('.').SinkMap.get('user');
+  }
+
+  function ServiceSink(prophash, client) {
+    ParentSink.call(this, prophash, client);
+  }
+  
+  ParentSink.inherit(ServiceSink, require('../methoddescriptors/serviceuser'));
+  ServiceSink.prototype.__cleanUp = function () {
+    ParentSink.prototype.__cleanUp.call(this);
+  };
+  return ServiceSink;
+}
+
+module.exports = createServiceSink;
+
+},{"../methoddescriptors/serviceuser":3}],7:[function(require,module,exports){
+function createUserSink(execlib, ParentSink) {
+  'use strict';
+  if (!ParentSink) {
+    ParentSink = execlib.execSuite.registry.get('.').SinkMap.get('user');
+  }
+
+  function UserSink(prophash, client) {
+    ParentSink.call(this, prophash, client);
+  }
+  
+  ParentSink.inherit(UserSink, require('../methoddescriptors/user'));
+  UserSink.prototype.__cleanUp = function () {
+    ParentSink.prototype.__cleanUp.call(this);
+  };
+  return UserSink;
+}
+
+module.exports = createUserSink;
+
+},{"../methoddescriptors/user":4}]},{},[1]);
