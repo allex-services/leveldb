afdd7d2e7e6d310401bd664b8f41151787a6875d
Veljko Popovic <v.s.popovic@gmail.com>
2016-06-21 13:02:14 +0200

    moving to new structure

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 function createServicePack(execlib) {
   'use strict';
 
-  var lib = execlib.lib,
-    q = lib.q,
-    d = q.defer(),
-    execSuite = execlib.execSuite;
 
-  execSuite.libRegistry.register('allex_leveldblib').done(
-    realCreator.bind(null, d),
-    d.reject.bind(d)
-  );
-
-  function realCreator(defer, leveldblib){
-    var ret = require('./clientside')(execlib),
-      execSuite = execlib.execSuite,
-      ParentServicePack = execSuite.registry.get('.');
-
-    ret.Service = require('./servicecreator')(execlib, ParentServicePack, leveldblib);
-    defer.resolve(ret);
-  }
-
-  return d.promise;
-  'use strict';
-  var ret = require('./clientside')(execlib),
-    execSuite = execlib.execSuite,
-    ParentServicePack = execSuite.registry.get('.');
-
-  ret.Service = require('./servicecreator')(execlib, ParentServicePack);
-  return ret;
+  return {
+    service : {
+      dependencies : ['allex:leveldb:lib']
+    },
+    sinkmap : {
+      dependencies: ['allex:leveldb:lib']
+    }
+  };
 }
 
 module.exports = createServicePack;
